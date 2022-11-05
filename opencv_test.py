d205039d5b2c7a4d5a66fd51e56aff5f4597f6dd
fyenne <fyenne@hotmail.com>
2022-11-05 20:44:38 +0800

    add

---
+++ opencv_test.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import os
+
+
+def find_circle(path):
+        
+    # Read image.
+    img = cv2.imread('./chines/data/data/input_1_1_5.jpg', cv2.IMREAD_COLOR)
+    print(5)
+    # Convert to grayscale.
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Blur using 3 * 3 kernel.
+    gray_blurred = cv2.blur(gray, (3, 3))
+    
+    # Apply Hough transform on the blurred image.
+    detected_circles = cv2.HoughCircles(gray_blurred, 
+                    cv2.HOUGH_GRADIENT, 1, 20, param1 = 50,
+                param2 = 30, minRadius = 1, maxRadius = 40)
+    
+    # Draw circles that are detected.
+    if detected_circles is not None:
+    
+        # Convert the circle parameters a, b and r to integers.
+        detected_circles = np.uint16(np.around(detected_circles))
+        
+        for pt in detected_circles[0, :]:
+            a, b, r = pt[0], pt[1], pt[2]
+    
+            # Draw the circumference of the circle.
+            cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+    
+            # Draw a small circle (of radius 1) to show the center.
+            cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+            cv2.imshow("Detected Circle", img)
+            cv2.waitKey(0)
+
+    else:
+        print('no circle in {path}'.format(path))
+
+if __name__ == "__main__":
+    print('start')
+    for i in os.listdir('./smaller_dataset/'):
+        print(i)
+        # find_circle()
+        break
